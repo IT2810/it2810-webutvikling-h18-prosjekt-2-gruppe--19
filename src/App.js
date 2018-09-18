8a2620d10c1373aab8148f8a958d88c1876c3e83
Unknown <paul.fossnes@gmail.com>
2018-09-18 15:50:19 +0200

    #5 Inserted Selector into App.js

--- src/App.js
+++ src/App.js
@@ -1,66 +1,44 @@
-import React, { Component } from 'react';
-import './App.css';
-import NavBar from './NavBar.js';
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./NavBar.js";
+import Selector from "./Selector.js";
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">
+            Fantastisk gruppeutstilling av gruppe 19
+          </h1>
+          <div className="Settings-button">
+            <span> v </span>
+          </div>
+        </header>
 
-				<header className="App-header">
-					<h1 className="App-title">Fantastisk gruppeutstilling av gruppe 19</h1>
-					<div className="Settings-button">
-						<span> v </span>
-					</div>
-				</header>
+        <section className="Filters">
+          <Selector />
+          <Selector />
+          <Selector />
+        </section>
 
-				<section className="Filters">
-					<div>
-						<form>
-							<fieldset>
-								<legend>Test</legend>
-								<input type="radio" name="text"/><label htmlFor="alt1">Alternativ 1</label>
-								<input type="radio" name="text"/><label htmlFor="alt2">Alternativ 2</label>
-								<input type="radio" name="text"/><label htmlFor="alt3">Alternativ 3</label>
-							</fieldset>
-						</form>
-					</div>
-					<div>
-						<form>
-							<fieldset>
-								<legend>Test</legend>
-								<input type="radio" name="text"/><label htmlFor="alt1">Alternativ 1</label>
-								<input type="radio" name="text"/><label htmlFor="alt2">Alternativ 2</label>
-								<input type="radio" name="text"/><label htmlFor="alt3">Alternativ 3</label>
-							</fieldset>
-						</form>
-					</div>
-					<div>
-						<form>
-							<fieldset>
-								<legend>Test</legend>
-								<input type="radio" name="text"/><label htmlFor="alt1">Alternativ 1</label>
-								<input type="radio" name="text"/><label htmlFor="alt2">Alternativ 2</label>
-								<input type="radio" name="text"/><label htmlFor="alt3">Alternativ 3</label>
-							</fieldset>
-						</form>
-					</div>
-				</section>
+        <section className="Art-section">
+          <NavBar />
 
-				<section className="Art-section">
-	                <NavBar/>
+          <div>
+            <img src="header.jpg" alt="Art 1" />
+            <p>
+              Lorem ipsum dolor sit amet, <br />
+              consectetur adipisicing elit, <br />
+              sed do eiusmod tempor incididunt ut labore et
+            </p>
+          </div>
 
-					<div>
-						<img src="header.jpg" alt="Art 1"/>
-						<p>Lorem ipsum dolor sit amet, <br />consectetur adipisicing elit, <br />sed do eiusmod tempor incididunt ut labore et</p>
-					</div>
-
-					<button>Play music</button>
-				</section>
-
-			</div>
-		);
-	}
+          <button>Play music</button>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
